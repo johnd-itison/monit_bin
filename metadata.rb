9b6151b4bc3a75375a3811e38f6479bd187d9099
John Daniels <john.daniels@itison.com>
2018-08-30 17:16:22 +0100

    Bump version of chef-sugar to fix deprecation warnings

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,9 @@ maintainer_email "sawanoboriyu@higanworks.com"
 license          "MIT"
 description      "Installs/Configures monit_bin"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "2.1.0"
+version          "2.1.1"
 supports         "ubuntu"
 supports         "smartos"
 supports         "debian"
 
-depends 'chef-sugar', '~> 3.4.0'
+depends 'chef-sugar', '~> 4.1.0'
